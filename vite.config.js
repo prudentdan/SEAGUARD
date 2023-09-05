e9b214c3c7d21d19aa1befa30b56942cbd35004d
Adedigba Oluwadarasimi <116315016+AdedigbaOluwad1@users.noreply.github.com>
2023-09-05 10:57:36 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    outDir: 'build', // Specify the output directory for the production build
+    minify: 'terser', // Minify the output using Terser
+    sourcemap: false, // Disable source maps in production
+  },
 })
